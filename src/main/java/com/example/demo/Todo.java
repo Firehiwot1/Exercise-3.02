69997297f49e772a484dae923a43a56a3671dcef
Firehiwot1 <fmazengia@gmail.com>
2019-07-17 14:17:49 -0400

    first initial

---
+++ src/main/java/com/example/demo/Todo.java
@@ -0,0 +1,40 @@
+package com.example.demo;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.Date;
+
+public class Todo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTaskName() {
+        return taskName;
+    }
+
+    public void setTaskName(String taskName) {
+        this.taskName = taskName;
+    }
+
+    public Date getDueDate() {
+        return dueDate;
+    }
+
+    public void setDueDate(Date dueDate) {
+        this.dueDate = dueDate;
+    }
+
+    private  String taskName;
+    private Date dueDate;
+
+}
